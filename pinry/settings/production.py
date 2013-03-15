113cb2e08c1859adc2b3cd7b5f60fd10809294e9
arctelix <arctelix@gmail.com>
2013-03-14 20:54:03 -0400

    test compress offline +
    add some comments for stuff

--- pinry/settings/production.py
+++ pinry/settings/production.py
@@ -17,11 +17,17 @@ DATABASES = {
     'PASSWORD': os.environ.get("DB_PASSWORD"),
   }
 }
-print 'DB_NAME: ', os.environ.get("DB_NAME")
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
+#Set SITE_PORT='' when port is :80
+SITE_PORT = ''
+SITE_URL = 'http://'+SITE_IP+SITE_PORT
+
 HOST_NAME = os.environ.get('HOST_NAME')
 print 'HOST_NAME: ', HOST_NAME
+print 'DB_NAME: ', os.environ.get("DB_NAME")
+
 DEFAULT_FILE_STORAGE = 'pinry.settings.s3utils.MediaRootS3BotoStorage'
 STATICFILES_STORAGE = 'pinry.settings.s3utils.StaticRootS3BotoStorage'
 #STATICFILES_STORAGE = 'pinry.settings.s3utils.CachedS3BotoStorage'
@@ -32,9 +38,6 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 print 'AWS_STORAGE_BUCKET_NAME: ', AWS_STORAGE_BUCKET_NAME
 S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 
-SITE_PORT = ''
-SITE_URL = 'http://'+SITE_IP+SITE_PORT
-
 MEDIA_URL = S3_URL+'media/'
 MEDIA_ROOT = S3_URL+'media/'
 STATIC_URL = S3_URL+'static/'
@@ -42,8 +45,8 @@ STATIC_ROOT = S3_URL+'static/'
 TMP_ROOT = os.path.join(SITE_ROOT, 'media/tmp/')
 TMP_URL = 'http://%s/media/tmp/' % HOST_NAME
 '''STATIC_PREFIX
-Static url can not be full url on local dev server so 
-this adds it to the bookmarklet. MUST BE = '' on production.
+Static url can not be full url on local dev server so STATIC_PREFIX 
+adds URL prefix to the bookmarklet. MUST BE = '' with remote static files.
 '''
 STATIC_PREFIX = ''
 
@@ -51,7 +54,7 @@ COMPRESS_ENABLED = True
 COMPRESS_STORAGE = STATICFILES_STORAGE 
 COMPRESS_URL = STATIC_URL
 COMPRESS_ROOT = STATIC_ROOT
-COMPRESS_OFFLINE = False
+COMPRESS_OFFLINE = True
 '''COMPRESS_OFFLINE issue:
 Compressing... Error: An error occured during rendering /app/pinry/pins/templates/pins/bmbase.html: '/static/vendor/boot
 strap/2.0.3/css/bootstrap.css' isn't accessible via COMPRESS_URL ('http://pinry.s3.amazonaws.com/static/') and can't be
